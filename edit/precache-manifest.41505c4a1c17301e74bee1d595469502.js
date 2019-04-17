39acf58d320ef64ec2957af2bc9469ed8254141c
Jacob Bremmer <jacob.bremmer@gmail.com>
2019-04-17 18:53:34 +0200

    updt

---
+++ edit/precache-manifest.41505c4a1c17301e74bee1d595469502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "48442b20cfdc5b91af59",
+    "url": "/static/js/main.3dd70e15.chunk.js"
+  },
+  {
+    "revision": "9461f9ecc80cf330f0c9",
+    "url": "/static/js/2.0d7d6026.chunk.js"
+  },
+  {
+    "revision": "48442b20cfdc5b91af59",
+    "url": "/static/css/main.2fecf0c6.chunk.css"
+  },
+  {
+    "revision": "9d983c939ce756918bb67915aaafefaf",
+    "url": "/index.html"
+  }
+];
